ab5effb84b4ffe47a426d6eb91f4b70ba18ab58d
陈胜 <263621221@qq.com>
2018-11-13 09:12:34 +0800

    自己改动

--- src/main/java/the/flash/handler/IMIdleStateHandler.java
+++ src/main/java/the/flash/handler/IMIdleStateHandler.java
@@ -11,6 +11,13 @@ public class IMIdleStateHandler extends IdleStateHandler {
     private static final int READER_IDLE_TIME = 15;
 
     public IMIdleStateHandler() {
+        /**
+         * 第一个表示读空闲时间，指的是在这段时间内如果没有数据读到，就表示连接假死；
+         * 第二个是写空闲时间，指的是 在这段时间如果没有写数据，就表示连接假死；
+         * 第三个参数是读写空闲时间，表示在这段时间内如果没有产生数据读或者写，就表示连接假死。写空闲和读写空闲为0，表示我们不关心者两类条件；
+         * 最后一个参数表示时间单位。
+         * 在我们的例子中，表示的是：如果 15 秒内没有读到数据，就表示连接假死...
+         */
         super(READER_IDLE_TIME, 0, 0, TimeUnit.SECONDS);
     }
 
